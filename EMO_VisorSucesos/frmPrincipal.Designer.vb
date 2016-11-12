fd13006bc4d68811928c4d529afe469679235f7c
José Juan Pérez <jjmurdoc68@hotmail.es>
2016-11-12 18:30:43 +0100

    Version Beta 1

---
+++ EMO_VisorSucesos/frmPrincipal.Designer.vb
@@ -0,0 +1,72 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmPrincipal
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.txtEventLog = New System.Windows.Forms.TextBox()
+        Me.ProgressBar1 = New System.Windows.Forms.ProgressBar()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'txtEventLog
+        '
+        Me.txtEventLog.Location = New System.Drawing.Point(106, 62)
+        Me.txtEventLog.Name = "txtEventLog"
+        Me.txtEventLog.Size = New System.Drawing.Size(185, 20)
+        Me.txtEventLog.TabIndex = 0
+        '
+        'ProgressBar1
+        '
+        Me.ProgressBar1.Location = New System.Drawing.Point(12, 361)
+        Me.ProgressBar1.Name = "ProgressBar1"
+        Me.ProgressBar1.Size = New System.Drawing.Size(664, 24)
+        Me.ProgressBar1.TabIndex = 1
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(440, 46)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(108, 35)
+        Me.Button1.TabIndex = 2
+        Me.Button1.Text = "Button1"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'frmPrincipal
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(688, 397)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.ProgressBar1)
+        Me.Controls.Add(Me.txtEventLog)
+        Me.Name = "frmPrincipal"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "EMO - Visor de Sucesos"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents txtEventLog As TextBox
+    Friend WithEvents ProgressBar1 As ProgressBar
+    Friend WithEvents Button1 As Button
+End Class
---
+++ EMO_VisorSucesos/frmPrincipal.vb
@@ -0,0 +1,53 @@
+﻿Public Class frmPrincipal
+    'Declaración de variables
+    Dim bolComprobacionPrevias As Boolean
+
+    Private Sub frmPrincipal_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Function fcnComprobacionesPrevias(strNombreMaquina As String) As Boolean
+        'Comprueba que existen los registros
+        fcnComprobacionesPrevias = True
+        If Not EventLog.Exists("System", "strNombreMaquina") Then
+            MsgBox("El Registro de Sistema no existe")
+            fcnComprobacionesPrevias = False
+        End If
+        If Not EventLog.Exists("Application", "strNombreMaquina") Then
+            MsgBox("El Registro de Aplicación no existe")
+            fcnComprobacionesPrevias = False
+        End If
+    End Function
+
+    Private Function fncLeerRegistro(strNombreRegistro As String, strNombreMaquina As String)
+        'Lee el registro indicado
+
+        'Declaración de variables
+        Dim eLog As EventLog = New EventLog
+        Dim elEntry As EventLogEntry
+        Dim lEntryActual As Long = 0
+
+        eLog.Log = strNombreRegistro
+        eLog.MachineName = strNombreMaquina
+
+        For Each elEntry In eLog.Entries
+            lEntryActual += 1
+            txtEventLog.Text = lEntryActual.ToString & " / " & eLog.Entries.Count.ToString
+            txtEventLog.Refresh()
+            ProgressBar1.Maximum = eLog.Entries.Count
+            ProgressBar1.Value = lEntryActual
+            'If elEntry.Source = Application.ProductName Then
+            '    MsgBox(elEntry.Message)
+            'End If
+        Next
+    End Function
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        'Comprobacion previas
+        bolComprobacionPrevias = fcnComprobacionesPrevias("INTRANET.EMO.ES.EMOSISSV15")
+        If bolComprobacionPrevias = True Then
+            'Lee el registro
+            fncLeerRegistro("Application", "INTRANET.EMO.ES.EMOSISSV15")
+        End If
+    End Sub
+End Class
